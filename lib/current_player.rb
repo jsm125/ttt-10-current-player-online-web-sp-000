47ac9987d7dd45fb86d776d95031d60c8e5cd992
Matt Im <matthew.seiim@gmail.com>
2020-02-02 00:36:13 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+
+counter = 0
+  board.each do |n|
+  if board == "X"
+  counter += 1
+  else
+end
+end
+end
+
+def current_player(board)
+
+end
+
+def move(board, index, character = "X")
+  board[index] = character
+end
